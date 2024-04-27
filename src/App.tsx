c4cfffe1d5cf602ff332ea6cd2f4872102e549e0
sleepielle <pggisselle@gmail.com>
2024-04-26 23:28:59 -0600

    Add pages and Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import Home from "./styles/pages/home/Home"
+import Users from "./styles/pages/users/users";
+import Products from "./styles/pages/products/products";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+         <Home/>
+      ),
+    },
+    {
+      path: "users",
+      element:
+       <Users/>
+    },
+    {
+      path: "products",
+      element:
+       <Products/>
+    },
+  ]);
+
+  return  <RouterProvider router={router} />;
+  
+ 
 }
 
 export default App
