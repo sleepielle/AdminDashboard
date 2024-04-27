e32ebce4b2faa372943a32f21c0ede9b300460bf
sleepielle <pggisselle@gmail.com>
2024-04-26 23:50:14 -0600

    Add Layouts and Pages

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss"
+
+const Footer =()=> {
+    return (
+        <div className="footer">
+            Footer
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,11 @@
+import "./Menu.scss"
+
+const Menu =()=> {
+    return (
+        <div className="Menu">
+            Menu
+        </div>
+    );
+}
+
+export default Menu;
