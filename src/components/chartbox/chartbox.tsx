9d53b6380bde56b23e22c43461f9e9019bc70611
sleepielle <pggisselle@gmail.com>
2024-05-03 11:56:32 -0600

    Adding rechart based on chartBox component

--- src/components/chartbox/chartbox.tsx
+++ src/components/chartbox/chartbox.tsx
@@ -1,7 +1,71 @@
 import "./chartbox.scss";
+import { Link } from "react-router-dom";
+import { ResponsiveContainer } from "recharts";
+import { LineChart } from "recharts";
+import { Line } from "recharts";
+import { XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 
-const Chartbox = () => {
-  return <div className="chartbox"></div>;
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const Chartbox = (props: Props) => {
+  return (
+    <div className="chartbox">
+      {/*primer hijo - primera columna*/}
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
+        </div>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>
+          View All
+        </Link>
+      </div>
+
+      {/*segundo hijo - segunda columna*/}
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 50 }}
+              />
+
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}
+          </span>
+          <span className="month">This month</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chartbox;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,11 @@
 import "./home.scss";
 import Topbox from "../../components/topbox/topbox";
+import Chartbox from "../../components/chartbox/chartbox";
+
+import { chartBoxUser } from "../../data";
+import { chartBoxProduct } from "../../data";
+import { chartBoxRevenue } from "../../data";
+import { chartBoxConversion } from "../../data";
 
 const Home = () => {
   return (
@@ -7,11 +13,20 @@ const Home = () => {
       <div className="box box1">
         <Topbox />
       </div>
-      <div className="box box2"></div>
-      <div className="box box3"></div>
+      <div className="box box2">
+        <Chartbox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <Chartbox {...chartBoxProduct} />
+      </div>
       <div className="box box4"></div>
-      <div className="box box5"></div>
-      <div className="box box6"></div>
+      <div className="box box5">
+        <Chartbox {...chartBoxRevenue} />
+      </div>
+      <div className="box box6">
+        {" "}
+        <Chartbox {...chartBoxConversion} />
+      </div>
       <div className="box box7"></div>
       <div className="box box8"></div>
       <div className="box box9"></div>
