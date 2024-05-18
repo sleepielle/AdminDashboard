0b4640c063793b6a03d73faff277703365a69607
sleepielle <pggisselle@gmail.com>
2024-05-18 00:33:42 -0600

    Finish Users section

--- src/components/dataTable/datatable.tsx
+++ src/components/dataTable/datatable.tsx
@@ -1,19 +1,45 @@
 import React from "react";
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./datatable.scss";
+import { Link } from "react-router-dom";
 
 type Props = {
   columns: GridColDef[];
   rows: object[];
+  slug: string;
 };
 
 const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {
+    //delete the item
+    //axios.delete(`/api/${slug}/id`)
+    console.log(id + "item has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+
   return (
     <div className="dataTable">
       <DataGrid
         className="dataGrid"
         rows={props.rows}
-        columns={props.columns}
+        columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -67,8 +67,8 @@ const Users = () => {
       <div className="info">
         <h1>Users</h1>
         <button>Add New User</button>
-        <DataTable columns={columns} rows={userRows} />
       </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
     </div>
   );
 };
