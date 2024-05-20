9af11c2a516424252fd951f22e64e622ad0914ce
sleepielle <pggisselle@gmail.com>
2024-05-20 04:59:47 -0600

    Finish Add Modal styling

--- src/components/AddModal/addmodal.tsx
+++ src/components/AddModal/addmodal.tsx
@@ -16,22 +16,24 @@ const AddModal = (props: Props) => {
   };
 
   return (
-    <div className="Add">
-      <span className="close" onClick={() => props.setOpen(false)}>
-        X
-      </span>
-      <h1>Add new {props.slug}</h1>
-      <form onSubmit={handleSubmit}>
-        {props.columns
-          .filter((item) => item.field != "id" && item.field != "img")
-          .map((column) => (
-            <div className="item">
-              <label>{column.headerName}</label>
-              <input type={column.type} placeholder={column.field} />
-            </div>
-          ))}
-        <button>Send</button>
-      </form>
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Add new {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field != "id" && item.field != "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 };
