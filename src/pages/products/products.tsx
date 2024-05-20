c89db96fcfd047f3bc953f929fb1e35177c0ce15
sleepielle <pggisselle@gmail.com>
2024-05-20 05:29:38 -0600

    Add Modal to Users and Products Page.

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,11 +1,73 @@
-import "./products.scss"
+import { useState } from "react";
+import "./products.scss";
+import AddModal from "../../components/AddModal/addmodal";
+import DataTable from "../../components/dataTable/datatable";
+import { userRows } from "../../data";
+import { GridColDef } from "@mui/x-data-grid";
+import { products } from "../../data";
 
-const Products =()=> {
-    return (
-        <div className="products">
-            Products
-        </div>
-    );
-}
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Image",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "title",
+    type: "string",
+    headerName: "Title",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Color",
+    width: 150,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Price",
+    width: 200,
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
+    type: "boolean",
+  },
+];
+
+const Products = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="products">
+      <div className="info">
+        <h1>Products</h1>
+        <button onClick={() => setOpen(true)}>Add New Product</button>
+      </div>
+      <DataTable slug="product" columns={columns} rows={products} />
+
+      {open && <AddModal slug="product" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
+};
 
 export default Products;
